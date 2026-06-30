6ff9e9c4053d00d02101d855056414e63e638ead
bisworanjanEphorsys2004 <bisworanjan.ephorsys@gmail.com>
2026-06-30 14:39:20 +0530

    admin auth creation and give access and otp verification system

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,13 +2,16 @@ import express from "express";
 import {
   forgotPassword,
   getAdminProfile,
+  giveAccess,
   loginAdmin,
   LogoutAdmin,
   registerAdmin,
   resetPassword,
+  getAllManagers,
+  toggleBlockManager,
+  deleteManager,
 } from "../controllers/admin.controller.js";
 import protect from "../middleware/auth.middleware.js";
-
 
 const router = express.Router();
 
@@ -47,5 +50,30 @@ router.post("/forgot-password", forgotPassword);
 // ------------------------------------------------------
 
 router.post("/reset-password", resetPassword);
+
+// ------------------------------------------------------
+// Manager creation
+// POST -> /api/v1/admin/give-access
+// Protect
+router.post("/give-access", protect, giveAccess);
+
+
+// ----------------------------------------------------
+// Get All Manager
+// GET -> /api/v1/admin/managers
+// Protect
+router.get("/managers", protect, getAllManagers);
+
+// ----------------------------------------------------
+// Toggle Block Manager
+// PATCH -> /api/v1/admin/managers/:id/block
+// Protect
+router.patch("/managers/:id/block", protect, toggleBlockManager);
+
+// ----------------------------------------------------
+// Delete Manager
+// DELETE -> /api/v1/admin/managers/:id
+// Protect
+router.delete("/managers/:id", protect, deleteManager);
 
 export default router;
